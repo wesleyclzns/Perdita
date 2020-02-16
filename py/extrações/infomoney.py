05f57a9947aba15fe97b4fd992b6b04489f34a13
wesleyclzns <wesclzns@gmail.com>
2020-02-16 14:43:51 -0300

    deletando firebase

--- py/extrações/infomoney.py
+++ py/extrações/infomoney.py
@@ -1,3 +1,5 @@
 'https://www.infomoney.com.br/cotacoes/empresas-b3/'
 
 'https://www.infomoney.com.br/cotacoes/itausa-itsa4/proventos/'
+
+'http://bvmf.bmfbovespa.com.br/indices/ResumoCarteiraQuadrimestre.aspx?Indice=IBOV&idioma=pt-br'
